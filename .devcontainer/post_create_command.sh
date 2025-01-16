33d86630f8e771c0022fa85221b37746183fa97a
Raphael Abenom <86538018+raphaelabenom@users.noreply.github.com>
2025-01-16 02:16:01 +0000

    .

--- .devcontainer/post_create_command.sh
+++ .devcontainer/post_create_command.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-pip install --user --upgrade pip
-pip install --user -r requirements.txt
+# Criar o ambiente virtual
+python -m venv .venv
+
+# Ativar o ambiente virtual
+# Para sistemas Unix (Linux, macOS):
+source .venv/bin/activate
+
+# Para sistemas Windows, use:
+# .\.venv\Scripts\activate
+
+# Atualizar pip
+pip install --upgrade pip
+
+# Instalar as dependências do arquivo requirements.txt
+pip install -r requirements.txt
